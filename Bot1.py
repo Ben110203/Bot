a575a3f0351f683fb4004403272ec2dae7fb2d59
Ben110203 <35886315+Ben110203@users.noreply.github.com>
2018-01-30 14:59:22 +0800

    bne

--- Bot1.py
+++ Bot1.py
@@ -17,8 +17,8 @@ replylist = ["剪刀","石頭","布"]
 app = Flask(__name__)
 
 # Replace by your channel secret and access token from Line Developers console -> Channel settings.
-LINE_CHANNEL_SECRET = 'd7cd6ac3a091291e21bd679b8bda9d54'
-LINE_CHANNEL_ACCESS_TOKEN = 'r+r82dIkwmOVnI43ehvHJaQbjUQl21tNGcoruT7aP1HxJSmeP1QTTALK3mkKjc+pxxoL8u/VInK27Ju/glylBpQ5p+ezqQoaCF76Xah3PhUm139iyRFG1wouMgff+e5H96nNfTQK0hU8CY31tq30/wdB04t89/1O/w1cDnyilFU='
+LINE_CHANNEL_SECRET = 'r+r82dIkwmOVnI43ehvHJaQbjUQl21tNGcoruT7aP1HxJSmeP1QTTALK3mkKjc+pxxoL8u/VInK27Ju/glylBpQ5p+ezqQoaCF76Xah3PhUm139iyRFG1wouMgff+e5H96nNfTQK0hU8CY31tq30/wdB04t89/1O/w1cDnyilFU='
+LINE_CHANNEL_ACCESS_TOKEN = 'd7cd6ac3a091291e21bd679b8bda9d54'
 line_bot_api = LineBotApi(LINE_CHANNEL_ACCESS_TOKEN)
 handler = WebhookHandler(LINE_CHANNEL_SECRET)
 
